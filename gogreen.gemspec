802cb928f02c5df1ed71c40e3811a3469072862c
James Robertson <digital.robertson@gmail.com>
2022-02-22 20:58:00 +0000

    minor improvement: Replaced RXFHelper with RXFReader.

--- gogreen.gemspec
+++ gogreen.gemspec
@@ -1,18 +1,18 @@
 Gem::Specification.new do |s|
   s.name = 'gogreen'
-  s.version = '0.3.2'
+  s.version = '0.4.0'
   s.summary = 'Run RSF jobs from the command line using a Dynarex flavoured ' + 
       'aliases file.'
   s.authors = ['James Robertson']
   s.files = Dir['lib/gogreen.rb']
-  s.add_runtime_dependency('rscript', '~> 0.8', '>=0.8.0')
-  s.add_runtime_dependency('dynarex', '~> 1.8', '>=1.8.27')
+  s.add_runtime_dependency('rscript', '~> 0.9', '>=0.9.0')
+  s.add_runtime_dependency('dynarex', '~> 1.9', '>=1.9.5')
   s.add_runtime_dependency('acronym', '~> 0.1', '>=0.1.4')
-  s.add_runtime_dependency('polyrex', '~> 1.3', '>=1.3.1')
+  s.add_runtime_dependency('polyrex', '~> 1.4', '>=1.4.0')
   s.add_runtime_dependency('optparse-simple', '~> 0.4', '>=0.4.5')
   s.signing_key = '../privatekeys/gogreen.pem'
   s.cert_chain  = ['gem-public_cert.pem']
   s.license = 'MIT'
-  s.email = 'james@jamesrobertson.eu'
+  s.email = 'digital.robertson@gmail.com'
   s.homepage = 'https://github.com/jrobertson/gogreen'
 end
--- lib/gogreen.rb
+++ lib/gogreen.rb
@@ -11,16 +11,16 @@ require 'tempfile'
 
 
 class Gg
-  
-  def self.call(argsx)    
-    puts GoGreen.new(argsx.first).execute(argsx[1], argsx[2..-1]).to_s    
-  end 
-  
-  def self.run(argsx, keepalive: false)    
+
+  def self.call(argsx)
+    puts GoGreen.new(argsx.first).execute(argsx[1], argsx[2..-1]).to_s
+  end
+
+  def self.run(argsx, keepalive: false)
     puts GoGreen.new(argsx.first, keepalive: keepalive)\
-        .execute(argsx[1], argsx[2..-1]).to_s    
-  end      
-  
+        .execute(argsx[1], argsx[2..-1]).to_s
+  end
+
 end
 
 class GoGreenException < Exception
@@ -28,19 +28,19 @@ end
 
 class GoGreen
 
-  def initialize(alias_file, site=nil, subdomain=nil, shell_execute: true, 
+  def initialize(alias_file, site=nil, subdomain=nil, shell_execute: true,
                  rsc: nil, keepalive: false)
     super()
 
     @shell_execute, @rsc, @alias_file = shell_execute, rsc, alias_file
     @keepalive = keepalive
 
-    buffer = RXFHelper.read(alias_file, auto: false).first
-    
+    buffer = RXFReader.read(alias_file).first
+
     name = case buffer
     when /^<\?dynarex/
       @records = dxread(buffer)
-    when /^<\?polyrex/ 
+    when /^<\?polyrex/
       @records, @conf = pxread(buffer, site, subdomain)
     else
       raise GoGreenException, 'alias file ' + alias_file + ' not recognised'
@@ -53,22 +53,22 @@ class GoGreen
 
     alias_found = @records[alias_name]
     return 'job not found' unless alias_found
-    
+
     cmd = alias_found[:body][:command]
     cmd += ' ' + @conf if @conf
-    
+
     if cmd[/^rcscript/] then
 
-      a = cmd.sub(/^rcscript /,'').split + job_args.map do |x| 
-        x.sub(/^["'](.*)["']$/,'\1') 
+      a = cmd.sub(/^rcscript /,'').split + job_args.map do |x|
+        x.sub(/^["'](.*)["']$/,'\1')
       end
 
       raw_code, args = RScript.new.read a
-    
+
       begin
 
         if @shell_execute then
-          
+
           a = raw_code.strip.lines
           a.unshift 'args = ' + args.inspect + "\n\n"
           lastline = a.pop
@@ -77,8 +77,8 @@ class GoGreen
           var = '$alias_file = ' + @alias_file.inspect
           code2 = "$0 = 'gogreen'\n" + var +"\n" + a.join.gsub('\"','"')\
               .gsub('\#','#')
-          
-          file = Tempfile.new('gogreen')        
+
+          file = Tempfile.new('gogreen')
           code3 = @keepalive ? apply_handler(code2) : code2
           file.write(code3)
           file.close
@@ -87,34 +87,34 @@ class GoGreen
           r.strip
           #pipe = IO.popen("ruby", "w")
           #pipe.write code2
-          #pipe.close        
-          
+          #pipe.close
+
         else
 
           code3 = @keepalive ? apply_handler(raw_code) : raw_code
           result = eval(code3)
 
         end
-        
+
       rescue
         ($!).to_s[/^[^\n]+/].to_s
-      end   
-   
+      end
+
     elsif cmd[/^rsc/] and @rsc
-      
+
       package, job, args = cmd.sub(/^rsc/,'').split + job_args
       @rsc.send(package.to_sym).send(job.to_sym, *args)
-      
-    else    
+
+    else
       `#{cmd} #{job_args.join(' ')}`
-    end      
-    
+    end
+
   end
 
   private
-  
+
   def apply_handler(s)
-    
+
     "begin
       #{s}
     rescue
@@ -131,7 +131,7 @@ class GoGreen
 
       dx2 = Dynarex.new.parse dx.summary[:include]
       dx2.records.merge(dx.records)
-    else    
+    else
       dx.records
     end
 
@@ -140,7 +140,7 @@ class GoGreen
 
   def pxread(buffer, site, subdomain)
 
-    px = Polyrex.new    
+    px = Polyrex.new
 
     px.parse buffer
 
@@ -154,7 +154,7 @@ class GoGreen
 
     r
   end
-  
+
 end
 
 if $0 == __FILE__ then
@@ -162,6 +162,6 @@ if $0 == __FILE__ then
   alias_file, alias_name, *job_args =  ARV
 
   gg = GoGreen.new alias_file
-  gg.execute alias_name, job_args  
+  gg.execute alias_name, job_args
 
 end
